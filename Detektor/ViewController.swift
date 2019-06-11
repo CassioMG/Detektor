a71ff8de2750d79fc60badf787f2d80b63a24e90
Cássio Marcos Goulart <cassiomgoulart@gmail.com>
2019-06-11 16:51:03 -0300

    Create project. Add Carthage dependencies.

---
+++ Detektor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Detektor
+//
+//  Created by Cássio Marcos Goulart on 11/06/19.
+//  Copyright © 2019 CMG Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
